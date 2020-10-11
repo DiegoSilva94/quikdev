48c4f52b7ba40b6dbe7d99f3ecee7bffbc08fe75
DSilva <dsilvaoliveira94@gmail.com>
2020-10-10 21:58:13 -0300

    ajuste no consumo da api para identificar erros

--- app/Service/TMDBApi.php
+++ app/Service/TMDBApi.php
@@ -4,11 +4,13 @@
 namespace App\Service;
 
 use Illuminate\Support\Facades\Http;
+use mysql_xdevapi\Exception;
 
 class TMDBApi
 {
     /**
      * @return array|mixed
+     * @throws \Exception
      */
     public static function getMovies()
     {
@@ -21,10 +23,13 @@ class TMDBApi
     /**
      * @param int $page
      * @return array|mixed
+     * @throws \Exception
      */
     protected static function getPopularMovies(int $page = 1)
     {
         $response = Http::get('https://api.themoviedb.org/3/movie/popular?api_key=' . env('TMDB_API_KEY') . '&language=en-US&page=' . $page);
+        if($response->failed())
+            throw new \Exception('Movies not found', 404);
         return $response->json();
     }
 
@@ -32,29 +37,38 @@ class TMDBApi
      * @param string $search
      * @param int $page
      * @return array|mixed
+     * @throws \Exception
      */
     protected static function getSearchMovies(string $search,int $page = 1)
     {
         $response = Http::get('https://api.themoviedb.org/3/search/movie?api_key=' . env('TMDB_API_KEY') . '&language=en-US&include_adult=false&page=' . $page. '&query=' . $search);
+        if($response->failed())
+            throw new \Exception('Movies not found', 404);
         return $response->json();
     }
 
     /**
      * @param int $id
      * @return array|mixed
+     * @throws \Exception
      */
     public static function getMovie(int $id)
     {
         $response = Http::get('https://api.themoviedb.org/3/movie/' . $id . '?api_key=' . env('TMDB_API_KEY') . '&language=en-US');
+        if($response->failed())
+            throw new \Exception('Movie not found', 404);
         return $response->json();
     }
 
     /**
      * @return array|mixed
+     * @throws \Exception
      */
     public static function getGenres()
     {
         $response = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=' . env('TMDB_API_KEY') . '&language=en-US');
+        if($response->failed())
+            throw new \Exception('Genres not found', 404);
         return $response->json();
     }
 }
